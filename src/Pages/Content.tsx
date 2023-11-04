21d33c0a371fccfa2d96eef95b1b4815ea09f645
afnanramadhan <13521011@std.stei.itb.ac.id>
2023-11-05 01:31:15 +0700

    add dump page

---
+++ src/Pages/Content.tsx
@@ -0,0 +1,13 @@
+
+import Navbar from "../components/Navbar";
+
+const Content = () => {
+    return (
+        <div>
+            <Navbar/>
+            <h1>Content</h1>
+        </div>
+    );
+};
+
+export default Content;
---
+++ src/Pages/Subscription.tsx
@@ -0,0 +1,13 @@
+
+import Navbar from "../components/Navbar"
+
+const Subscription = () => {
+    return (
+        <div>
+            <Navbar/>
+            <h1>Subscription</h1>
+        </div>
+    )
+}
+
+export default Subscription
