682352edfdc090011e5900244486f9df643d74d9
kelvinra <13521005@mahasiswa.itb.ac.id>
2023-11-13 18:52:42 +0700

    fix: navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import logo from '../assets/NutriCraft.svg'
 import defaultPict from "../assets/default.svg"
+import { Link } from 'react-router-dom';
 
 
 const Navbar = () =>{
@@ -34,20 +35,20 @@ const Navbar = () =>{
         <div className='bg-[#10A5A5] h-full w-full font-sans'>
            <div className="hidden md:flex items-center justify-between">
                 <div className='flex flex-row space-x-9 justify-start items-center font-bold cursor-pointer transform text-xl'>
-                    <a href="/">
+                    <Link to="/">
                         <img src={logo} alt="" className='flex w-[174px] h-[55px] ml-[25px]'/>
-                    </a>
-                    <a href="/">
+                    </Link>
+                    <Link to="/">
                         <button type="button" className="home hover:scale-125">Home</button>
-                    </a>
-                    <a href="/forum">
+                    </Link>
+                    <Link to="/forum">
                         <button type="button" className="meals hover:scale-125">Forum</button>
-                    </a>
-                    <a href="/Content">
+                    </Link>
+                    <Link to="/content">
                         <button type="button" className="fact hover:scale-125">Content</button>
-                    </a>
+                    </Link>
                 </div>
-                <div className='flex flex-row justify-end items-center'>
+                {/* <div className='flex flex-row justify-end items-center'>
                     <div onMouseLeave={() => setOpen(false)} className="relative">
                         <button onMouseOver={() => setOpen(true)} className="meals bg-[#FF6B00] border-none rounded-2xl w-[154px] h-[35px] text-white cursor-pointer hover:bg-[#ff6a00cc]">{textLogin}</button>
                         <ul className={`absolute right-0 w-40 drop-shadow-lg ${open&&login ? "block" : "hidden"}`}>
@@ -62,7 +63,15 @@ const Navbar = () =>{
                         </ul>
                     </div>
                     <img src={defaultPict} alt="" className='h-[40px] m-5 '/>
-                </div>
+                </div> */}
+                <Link to="/login">
+                <button
+    type="button"
+    className="login bg-[#EF4800] border-none rounded-[30px] w-[154px] h-[35px] text-white text-center text-lg font-bold cursor-pointer mr-8 transition duration-300 hover:bg-[#FF6B00] transform scale-110 shadow-md"
+>
+    Login
+</button>
+                </Link>
             </div>
             
             <div className='items-center justify-between flex  md:hidden'>
