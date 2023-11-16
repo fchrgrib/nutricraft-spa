aa4d1f9a65e4e3769d3b571b64e3658bb4856e4e
kelvinra <13521005@mahasiswa.itb.ac.id>
2023-11-16 18:54:32 +0700

    feat: add profil level bar and list post

--- src/Pages/Profile.tsx
+++ src/Pages/Profile.tsx
@@ -4,6 +4,10 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 import {jwtDecode} from "jwt-decode";
 import Navbar from '../components/Navbar';
+import { get } from 'http';
+import { profile } from 'console';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 interface userJwt{
     uuid: string
@@ -13,6 +17,7 @@ interface userJwt{
 }
 
 interface ProfileData {
+  id: number;
   name: string;
   email: string;
   title: string;
@@ -22,10 +27,27 @@ interface ProfileData {
   // add any other fields you need for the user's profile
 }
 
+interface LevelBarProps {
+    level: number;
+    xp: number;
+}
+
+
+
+
 const Profile: React.FC = () => {
   const [userInfo, setUserInfo] = useState<userJwt|null>(null)
-  const [profileData, setProfileData] = useState<ProfileData|null>(null);
+  const [profileData, setProfileData] = useState<ProfileData>({
+    id: 0,
+    name: '',
+    email: '',
+    title: '',
+    phone: '',
+    description: '',
+    password: '',
+  });
   const [isEdit, setIsEdit] = useState<boolean>(false);
+  const [photo, setPhoto] = useState('');
 
 
 
@@ -33,23 +55,72 @@ const Profile: React.FC = () => {
 
   const host = process.env.URL||'http://localhost:8080'
 
-  
-  
-  
-  
   const fetchProfileData = async (uuid:string) => {
-    // const response = await axios.get(`${host}/user/${uuid}`);
-    // const data = await response.data;
+    const response = await axios.post(`${host}/user`, {
+      uuid: uuid
+    }, {withCredentials: true})
+    console.log(response.data.data)
     const data = {
-        name: "Kelvin Rayhan",
-        email: "ajikule@gmail.com",
-        title: "Software Engineer",
-        phone: "081234567890",
-        description: "I am a software engineer with 5 years of experience",
-        password: "12345678",
+        id: response.data.data.id,
+        name: response.data.data.name,
+        email: response.data.data.email,
+        title: response.data.data.title,
+        phone: response.data.data.phone_number,
+        description: response.data.data.description,
+        password: response.data.data.password,
     }
     setProfileData(data);
-  }
+}
+
+
+
+    const fetchPhoto = async (uuid:string) => {
+        return await axios.get(`${host}/image/profile/${uuid}`)
+            .then(response => {
+                setPhoto(response.data.url)
+            }).catch(e => {
+                console.log(e)
+            })
+    }
+    
+    const Subscriber = () => {
+        const [subscriber , setSubscriber] = useState('0');
+
+        return (
+            <div className='mt-15 bg-[#EF4800] rounded-xl'>
+                <div className="m-15 flex flex-row items-center justify-center gap-5 text-white">
+                    <div className="flex flex-col items-center justify-center">
+                        <p className="text-2xl font-bold">{subscriber}</p>
+                        <p className="text-lg font-bold">Subscriber</p>
+                    </div>
+                    <div className="flex flex-col items-center justify-center">
+                        <p className="text-2xl font-bold">0</p>
+                        <p className="text-lg font-bold">Content</p>
+                    </div>
+                </div>
+            </div>
+        )
+        
+    }
+
+
+const LevelBar: React.FC<LevelBarProps> = ({xp}) => {
+    const currentLevel = Math.floor(xp / 100);
+    const currentPercentage = xp - (currentLevel * 100)
+
+    return (
+        <div>
+            <h3 className="text-lg font-bold">Level {currentLevel}</h3>
+            <div className="relative w-full h-4 bg-gray-300 rounded-full">
+                <div
+                    className="absolute h-full bg-[#EF4800] rounded-full"
+                    style={{ width: `${currentPercentage}%` }}
+                ></div>
+            </div>
+        </div>
+    );
+};
+    
 
   useEffect(() => {
     const token = Cookies.get('token');
@@ -68,7 +139,49 @@ const Profile: React.FC = () => {
     }, [userInfo]);
 
 
-    
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setProfileData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+    const handleChangedesc = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setProfileData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+
+
+    const handleSave = async () => {
+        if (profileData.name === "") {
+        }
+        
+    }
+
+
+    const PostCard = () => {
+        return (
+            <div className='flex flex-row justify-between pr-5 gap-5 w-[100%]'>
+                <div className='flex flex-row'>
+                    <p className='text-[20px]'>1</p>
+                    <div className='flex flex-col gap-5'>
+                        <p className='text-[20px] font-bold'>Title</p>
+                        <p className='text-[16px]'>Body</p>
+                    </div>
+                </div>
+                <div className='buttons delete'>
+                    <i className="fas fa-trash-alt"></i>
+                    <FontAwesomeIcon icon={faTrashAlt} />
+                </div>
+            </div>
+        )
+    }
     
     const buttons = () => {
       if (!isEdit) {
@@ -98,6 +211,7 @@ const Profile: React.FC = () => {
           document.getElementById("cancel")!.style.display = "none";
           document.getElementById("edit")!.style.display = "block";
           document.getElementById("delete")!.style.display = "block";
+          fetchProfileData(userInfo!.uuid)
           
       }
       setIsEdit(!isEdit);
@@ -110,59 +224,76 @@ const Profile: React.FC = () => {
     <div>
         <Navbar/>
         <div>
-            <div className="flex flex-col items-center justify-center w-screen" style={{ height: 'calc(100vh - 115px)' }}>
-                <div className="profileContainer flex flex-row items-start gap-12  border border-[#EF4800] rounded-xl p-10">
-                    <div className="profileImage">
-                        <img src="https://i.imgur.com/8Km9tLL.png" alt="profile" className='h-[200px] w-[200px]' />
-
-                    </div>
-                    <div className='profileData flex flex-col justify-center items-start'>
-                        <p className="labelProfile">Name</p>
-                        <div>
-                            <input type="text" placeholder={profileData.name} className="textField bg-white border-none w-[420px] h-35 text-black no-underline text-16 font-semibold  shadow-none outline-none  mb-2" id="name" name="name" disabled/>
-                            <i className="fas fa-edit editIcon" ></i>
+            <div className="flex flex-col items-center justify-start w-[95vw] pt-10 ml-10">
+                <div className="profileContainer flex flex-col items-center gap-12  border border-[#EF4800] rounded-xl p-10 mb-10">
+                    <h1 className='text-[30px] font-extrabold'>Profile</h1>
+                    <div className='flex flex-row items-start gap-12'>
+                        <div className="profileImage flex flex-col gap-20">
+                            <img src={photo} alt="profile" className='h-[200px] w-[200px]' />
+                            {/* <Subscriber/> */}
+                            <LevelBar level={1} xp={325} />
                         </div>
-                        <hr/>
-                        <p className="labelProfile">Email</p>
-                        <div id="emailcontainer">
-                            <input type="email" placeholder={profileData.email} className="textField bg-white border-none w-[420px] h-35 text-black no-underline text-16 font-semibold  shadow-none outline-none  mb-2" id="email" name="email" disabled/>
-                            <i className="fas fa-edit editIcon" ></i>
-                            <p id="emailInvalid"></p>
-                        </div>
-                        <hr/>
-                        <p className="labelProfile">Title</p>
-                        <div>
-                            <input type="text" placeholder={profileData.title} className="textField bg-white border-none w-[420px] h-35 text-black no-underline text-16 font-semibold  shadow-none outline-none  mb-2" id="title" name="title" disabled/>
-                            <i className="fas fa-edit editIcon"></i>
-                        </div>
-                        <hr/>
-                        <p className="labelProfile">Description</p>
-                        <div>
-                            <textarea placeholder={profileData.description} className="textField max-h-[200px] min-h-[20px] bg-white border-none w-[420px] h-35 text-black no-underline text-16 font-semibold  shadow-none outline-none  mb-2" id="description" name="description" disabled/>
-                            <i className="fas fa-edit editIcon"></i>
-                        </div>
-                        <p className="labelProfile">Phone Number</p>
-                        <div id="phonenumbercontainer">
-                            <input type="text" placeholder={profileData.phone} className="textField bg-white border-none w-[420px] h-35 text-black no-underline text-16 font-semibold  shadow-none outline-none  mb-2" id="phoneNumber" name="phone"  disabled/>
-                            <i className="fas fa-edit editIcon"></i>
-                            <p id="phoneNumberInvalid"></p>
-                        </div>
-                        <hr/>
-                        <p className="labelProfile">Password</p>
-                        <div>
-                            <input type="password" placeholder={profileData.password} className="textField bg-white border-none w-[420px] h-35 text-black no-underline text-16 font-semibold  shadow-none outline-none  mb-2" id="editPassword"  name="password" disabled/>
-                            <i className="fas fa-edit editIcon"></i>
-                        </div>
-                        <hr/>
-                        <div className="flex flex-row justify-center items-center gap-5 mt-5">
-                        <button type="button" className="editButton bg-[#EF4800] border-none rounded-[30px] w-[120px] h-[30px] text-white text-center text-lg font-bold cursor-pointer mr-8 transition duration-300 hover:bg-[#FF6B00] transform scale-110 shadow-md" name="edit" id="edit" onClick={buttons} >Edit</button>
-                        <button type="button" className="saveButton bg-[#EF4800] border-none rounded-[30px] w-[120px] h-[30px] text-white text-center text-lg font-bold cursor-pointer mr-8 transition duration-300 hover:bg-[#FF6B00] transform scale-110 shadow-md" style={{display: 'none' }} name="save" id="save">Save</button>
-                        <button type="button" className="deleteButton bg-[red] border-none rounded-[30px] w-[120px] h-[30px] text-white text-center text-lg font-bold cursor-pointer mr-8 transition duration-300 hover:bg-[#FF6B00] transform scale-110 shadow-md" name="delete" id="delete">Delete</button>
-                        <button type="button" className="cancelButton bg-white rounded-[30px] w-[120px] h-[30px] text-[#EF4800]  border border-[#EF4800] text-center text-lg font-bold cursor-pointer mr-8 transition duration-300 hover:bg-[#FF6B00] hover:text-white transform scale-110 shadow-md" style={{display: 'none' }} onClick={buttons} name="cancel" id="cancel">Cancel</button>
+                        <div className='profileData flex flex-col justify-center items-start'>
+                            <p className="labelProfile">Name</p>
+                            <div className='flex'>
+                                <input type="text" value={profileData.name} className="textField bg-white border-none w-[90%] h-35 text-black no-underline text-16 font-semibold  shadow-none outline-none  mb-2" id="name" name="name" disabled onChange={handleChange}/>
+                                <i className="fas fa-edit editIcon" ></i>
+                            </div>
+                            <hr/>
+                            <p className="labelProfile">Email</p>
+                            <div id="emailcontainer" className='flex'>
+                                <input type="email" value={profileData.email} className="textField bg-white border-none w-[90%] h-35 text-black no-underline text-16 font-semibold  shadow-none outline-none  mb-2" id="email" name="email" disabled onChange={handleChange}/>
+                                <i className="fas fa-edit editIcon" ></i>
+                                <p id="emailInvalid"></p>
+                            </div>
+                            <hr/>
+                            <p className="labelProfile">Title</p>
+                            <div className='flex'>
+                                <input type="text" value={profileData.title} className="textField bg-white border-none w-[90%] h-35 text-black no-underline text-16 font-semibold  shadow-none outline-none  mb-2" id="title" name="title" disabled onChange={handleChange}/>
+                                <i className="fas fa-edit editIcon"></i>
+                            </div>
+                            <hr/>
+                            <p className="labelProfile">Description</p>
+                            <div className='flex'>
+                                <textarea value={profileData.description} className="textField max-h-[200px] min-h-[20px] bg-white border-none w-[90%] h-35 text-black no-underline text-16 font-semibold  shadow-none outline-none  mb-2" id="description" name="description" disabled onChange={handleChangedesc}/>
+                                <i className="fas fa-edit editIcon"></i>
+                            </div>
+                            <p className="labelProfile">Phone Number</p>
+                            <div id="phonenumbercontainer" className='flex'>
+                                <input type="text" value={profileData.phone} className="textField bg-white border-none w-[90%] h-35 text-black no-underline text-16 font-semibold  shadow-none outline-none  mb-2" id="phoneNumber" name="phone"  disabled onChange={handleChange}/>
+                                <i className="fas fa-edit editIcon"></i>
+                                <p id="phoneNumberInvalid"></p>
+                            </div>
+                            <hr/>
+                            <p className="labelProfile">Password</p>
+                            <div className='flex'>
+                                <input type="text" value={profileData.password} className="textField bg-white border-none w-[90%] h-35 text-black no-underline text-16 font-semibold  shadow-none outline-none  mb-2" id="editPassword"  name="password" disabled onChange={handleChange}/>
+                                <i className="fas fa-edit editIcon"></i>
+                            </div>
+                            <hr/>
+                            <div className="flex flex-row justify-center items-center gap-5 mt-5">
+                            <button type="button" className="editButton bg-[#EF4800] border-none rounded-[30px] w-[120px] h-[30px] text-white text-center text-lg font-bold cursor-pointer mr-8 transition duration-300 hover:bg-[#FF6B00] transform scale-110 shadow-md" name="edit" id="edit" onClick={buttons} >Edit</button>
+                            <button type="button" className="saveButton bg-[#EF4800] border-none rounded-[30px] w-[120px] h-[30px] text-white text-center text-lg font-bold cursor-pointer mr-8 transition duration-300 hover:bg-[#FF6B00] transform scale-110 shadow-md" style={{display: 'none' }} name="save" id="save">Save</button>
+                            <button type="button" className="deleteButton bg-[red] border-none rounded-[30px] w-[120px] h-[30px] text-white text-center text-lg font-bold cursor-pointer mr-8 transition duration-300 hover:bg-[#FF6B00] transform scale-110 shadow-md" name="delete" id="delete">Delete</button>
+                            <button type="button" className="cancelButton bg-white rounded-[30px] w-[120px] h-[30px] text-[#EF4800]  border border-[#EF4800] text-center text-lg font-bold cursor-pointer mr-8 transition duration-300 hover:bg-[#FF6B00] hover:text-white transform scale-110 shadow-md" style={{display: 'none' }} onClick={buttons} name="cancel" id="cancel">Cancel</button>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
+                <div className='PostContainer flex flex-col items-center justify-center gap-5 p-10 border border-[#EF4800] rounded-xl mb-10 w-[50%]'>
+                    <h1 className='text-[30px] font-extrabold'>Post</h1>
+                    <div className='flex flex-col items-start gap-10 overflow-y-auto w-[100%] max-h-[200px]'>
+                        <PostCard/>
+                        <PostCard/>
+                        <PostCard/>
+                        <PostCard/>
+                        <PostCard/>
+                        <PostCard/>
+                        <PostCard/>
+                        <PostCard/>
+                    </div>
+                </div>
+            </div>    
         </div>
     </div>
   );
